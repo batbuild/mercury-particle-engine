c57f0b838df3c0c5880b6969b9da4b94b361c70d
Andrew O'Connor <bravesirandrew@gmail.com>
2014-02-25 19:00:40 +0000

    Added missing test file.

---
+++ Mercury.ParticleEngine.Core.Tests/Profiles/ProfilesTestHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace Mercury.ParticleEngine.Profiles
+{
+	public static class ProfilesTestHelper
+	{
+		public static Particle CreateParticleStructure()
+		{
+			var particles = new Particle
+			{
+				X = new float[1],
+				Y = new float[1],
+				VX = new float[1],
+				VY = new float[1],
+			};
+			return particles;
+		}
+	}
+}
