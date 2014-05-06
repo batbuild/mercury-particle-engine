4bfdfaa4ae6f304d8140541eed883d08a7b067db
Andrew O'Connor <bravesirandrew@gmail.com>
2014-05-06 09:31:03 +0100

    Added RotateToFaceDirectionModifier, a modifier that will rotate a particle so it faces its direction of travel. Good for sparks.

---
+++ Mercury.ParticleEngine/Modifiers/RotateToFaceDirectionModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mercury.ParticleEngine.Modifiers
+{
+	public class RotateToFaceDirectionModifier : Modifier
+	{
+		protected internal override unsafe void Update(float elapsedSeconds, ref Particle particle, int count)
+		{
+			const float piOver2 = (float)Math.PI / 2f;
+
+			while (count-- > 0)
+			{
+				particle.Rotation[count] = (float) Math.Atan2(particle.VY[count], particle.VX[count]);
+			}
+		}
+	}
+}
